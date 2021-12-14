5e52ecd4cd4c7a4f5fc6dd36b34cc16a0cff574d
kanoi-y <furafuratonbo@gmail.com>
2021-12-14 15:48:38 +0900

    ✨ ogp/index.tsを作成

---
+++ src/pages/api/ogp/index.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import * as playwright from 'playwright-aws-lambda';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StoryOgpTemplate } from '../../../components/StoryOgpTemplate';
+
+
+ const getOgp =  async (req: NextApiRequest, res: NextApiResponse) => {
+  const { title, teamName } = req.query;
+  const viewport = { width: 1200, height: 630 };
+
+  const browser = await playwright.launchChromium();
+  const page = await browser.newPage({ viewport });
+
+  const props = { title: title as string, teamName: teamName as string };
+  const element = React.createElement(StoryOgpTemplate, props);
+  const markup = ReactDOMServer.renderToStaticMarkup(element);
+  const html = `<!doctype html>${markup}`;
+
+  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+
+  const image = await page.screenshot({ type: 'png' });
+  await browser.close();
+
+  res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate');
+
+  res.setHeader('Content-Type', 'image/png');
+
+  res.end(image);
+ };
+
+export default getOgp;
