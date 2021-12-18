694a204f750a242990863a9bd44eb058853dfb74
itizawa <itizawa.pen@gmail.com>
2021-12-18 18:14:23 +0900

    🚨 fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-   <div><h1>Hello World!</h1></div>
-  )
+    <div>
+      <h1>This is OGP Server for Proeco!</h1>
+      <ul>
+        <li>
+          Github:
+          <a href="https://github.com/proeco/proeco-ogp">https://github.com/proeco/proeco-ogp</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
